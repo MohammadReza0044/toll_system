cc89801c8833d347e8c0c1027eda5d51fcb92635
MohammadReza0044 <mohammadreza.shayegan@gmail.com>
2023-04-12 18:38:27 +0430

    folders srtucture are done. api app has been added.

---
+++ Backend/API/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from .models import Car, Owner, Road, Toll, TollStaion, Traffic
+
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = "__all__"
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Owner
+        fields = "__all__"
+
+
+class RoadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Road
+        fields = "__all__"
+
+
+class TollStaionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TollStaion
+        fields = "__all__"
+
+
+class TrafficSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Traffic
+        fields = "__all__"
+
+
+class TollSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Toll
+        fields = ["car", "toll_station", "date"]
